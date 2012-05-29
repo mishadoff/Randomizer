9c7b375fe73649d7c3e963cadd706062588b8547
mishadoff <mishadoff@gmail.com>
2012-05-30 00:02:19 +0300

    Dice layout changed

--- src/com/mishadoff/randomizer/DiceActivity.java
+++ src/com/mishadoff/randomizer/DiceActivity.java
@@ -1,7 +1,10 @@
 package com.mishadoff.randomizer;
 
 import android.app.Activity;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.View;
 
 public class DiceActivity extends Activity {
 	@Override
@@ -9,4 +12,8 @@ public class DiceActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.diceview);
     }
+	
+	public void diceGenerateHandler(View view){
+		// TODO to be implemented
+	}
 }
