8b0897f119832722d0fbfc0970c1cf5c6645d8e1
mishadoff <mishadoff@gmail.com>
2013-03-16 13:29:01 +0200

    Add Preferences

--- src/com/mishadoff/randomizer/activities/SettingsActivity.java
+++ src/com/mishadoff/randomizer/activities/SettingsActivity.java
@@ -1,16 +1,14 @@
 package com.mishadoff.randomizer.activities;
 
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
 import com.mishadoff.randomizer.R;
 
-import android.app.Activity;
-import android.os.Bundle;
-
-public class SettingsActivity extends Activity{
-	
+public class SettingsActivity extends PreferenceActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-        setContentView(R.layout.settingsview);
+		addPreferencesFromResource(R.xml.preferences);
 	}
-
 }
